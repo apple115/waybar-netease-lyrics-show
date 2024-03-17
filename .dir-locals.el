d10f8a38640fb4474b3d1b282c4357cff81489a0
apple115 <2567031030@qq.com>
2024-03-17 15:44:40 +0800

    update README

--- .dir-locals.el
+++
@@ -1,3 +0,0 @@
-((nil . ((projectile-project-test-cmd . "pytest ./test_song.py")
-         (projectile-project-run-cmd . "python ./song.py")
-)))
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # waybar-netease-lyrics-show
 display lyrics on waybar 
+
+这是一个简单的的实验项目
+在waybar 上显示歌词 目前只支持Qcm 
+
