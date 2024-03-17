ecc10ba817c3d7a49547f63d361c794b01324048
apple115 <2567031030@qq.com>
2024-03-17 14:52:54 +0800

    Improved robustness

--- song.py
+++ song.py
@@ -7,38 +7,42 @@ import time
 # TODO: 将这个输出字符串的地方进行独立
 # TODO： 添加args 利于这个代码调试
 
-SUPPERTED_PLAYER=['Qcm']
+SUPPORTED_PLAYER = ["Qcm"]
 
-def get_player_name(SUPPERTED_PLAYER):
-    if SUPPERTED_PLAYER==None:
-        raise ValueError("suppert player is none")
-    command = f'playerctl --list-all'
-    result = subprocess.check_output(command,shell=True,text=True).strip()
-    current_play=next((player for player in SUPPERTED_PLAYER if player in result),None)
+
+def get_player_name(SUPPORTED_PLAYER):
+    """
+    return player
+
+    Args:
+    SUPPORTED_PLAYER is  all support song player
+
+    Return:
+      current_play | none
+    """
+    if SUPPORTED_PLAYER == None:
+        raise ValueError("support player is none")
+    command = f"playerctl --list-all"
+    result = subprocess.check_output(command,stderr=subprocess.STDOUT,shell=True,text=True)
+    result = result.strip()
+    current_play = next(
+        (player for player in SUPPORTED_PLAYER if player in result), None
+    )
     # current_play=None
-    # for player in SUPPERTED_PLAYER:
+    # for player in SUPPORTED_PLAYER:
     #     if player in result:
     #         current_play = player
     #         break
-    if current_play is None:
-        raise ValueError("no suppert player")
     return current_play
 
-#print(get_player_name(SUPPERTED_PLAYER))
-
-def getPlayShell():
-    pass
+#get_player_name(SUPPORTED_PLAYER)
 
 
-playerName = os.popen("playerctl --list-all |  head -n 1").read().strip()
-# playerShell=f'playerctl --player={playerName}'
-playerShell = f"playerctl --player=Qcm"
-
-
-def getSongId():
+def get_song_id(playerName):
     """
     返回songid
     """
+    playerShell = f"playerctl --player={playerName}"
 
     command = (
         f"{playerShell} metadata mpris:trackid | cut -d '/' -f3 | cut -d \"'\" -f1"
@@ -47,27 +51,30 @@ def getSongId():
     return result.strip()
 
 
-def getSongTitle():
+
+
+def get_song_title(playerName):
     """
     返回songtitle
     """
-
+    playerShell = f"playerctl --player={playerName}"
     command = f"{playerShell} metadata title"
     result = subprocess.check_output(command, shell=True, text=True)
     return result.strip()
 
 
-def get_playing_song_position():
+def get_playing_song_position(playerName):
     """
     返回song位置
     """
-
+    playerShell = f"playerctl --player={playerName}"
     command = f"{playerShell} metadata --format '{{{{ duration(position) }}}}'"
     result = subprocess.check_output(command, shell=True, text=True)
     return result.strip()
 
 
-def getSongLyricsbyApi(id):
+
+def get_song_lyrics_by_api(id):
     """
     这个函数通过读取到网易云的歌词api
     Args：
@@ -84,20 +91,20 @@ def getSongLyricsbyApi(id):
     return result
 
 
-def getSongLyricsByFile(songId, SongTitle):
+def get_song_lyrics_by_file(songId, SongTitle):
     # 如果当前目录没有lyricfiles 则创造一个文件夹
     if not os.path.exists("lyricfiles"):
         os.makedirs("lyricfiles")
-        print("Created directory: lyricfiles")
+        # print("Created directory: lyricfiles")
     # 如果lyricfiles里 没有songTitle+songid的歌词文件
     # 则添加这个“songTitlesongid.lrc”
     lyric_file = f"./lyricfiles/{SongTitle}{songId}.lrc"
     if not os.path.exists(lyric_file):
         attempts = 0
-        lyric = getSongLyricsbyApi(songId)
+        lyric = get_song_lyrics_by_api(songId)
         while lyric == "" and attempts < 3:
             time.sleep(1)
-            lyric = getSongLyricsbyApi(songId)
+            lyric = get_song_lyrics_by_api(songId)
             attempts += 1
         if lyric == "":
             raise ValueError(f"API没有返回歌词 歌词id：{songId}")
@@ -112,7 +119,6 @@ def getSongLyricsByFile(songId, SongTitle):
     return lyric
 
 
-# print(get_playing_song_position())
 
 
 def get_song_current_lyric_from_lyrics_by_current_position(lyrics, current_position):
@@ -141,28 +147,50 @@ def get_song_current_lyric_from_lyrics_by_current_position(lyrics, current_posit
         return None
 
 
+lyrics_cache = {}
+
+
 # TODO：优化这个代码
 def song():
-    # print(title)
-    currentLyric = ""
-    lyric = ""
-    while True:
-        title = getSongTitle()
-        oldtitle = ""
-        if title != "":
-            songId = getSongId()
-            current_position = get_playing_song_position()
-            if title != oldtitle or lyric == "":
-                lyric = getSongLyricsByFile(songId, title)
-                oldtitle = title
-            result = get_song_current_lyric_from_lyrics_by_current_position(
-                lyric, current_position
-            )
-            currentLyric = result if result is not None else currentLyric
-        print(currentLyric, flush=True)
-        time.sleep(1)
+    """
+    Args:
+
+    Return:
+    player_name is None  return string'no song player'player
+    else return currentLyric
+
+    """
+    global lyrics_cache
+
+    player_name = get_player_name(SUPPORTED_PLAYER)
+    if player_name is None:
+        return f"no song player"
+    title = get_song_title(player_name)
+    if title == "":
+        return f"song is not play"
+    songId = get_song_id(player_name)
+    current_position = get_playing_song_position(player_name)
+    if title in lyrics_cache:
+        lyric = lyrics_cache[title]
+    else:
+        lyric = get_song_lyrics_by_file(songId, title)
+        # 将获取的歌词存入缓存
+        lyrics_cache[title] = lyric
+
+    result = get_song_current_lyric_from_lyrics_by_current_position(
+        lyric, current_position
+    )
+
+    currentLyric = result if result is not None else ""
+    return currentLyric
+
 
 def print_string():
-    pass
+    current_string = "current_string is None"
+    while True:
+        current_string = song()
+        print(current_string, flush=True)
+        time.sleep(1)
 
-# song()
+if __name__ == '__main__':
+    print_string()
--- test_song.py
+++ test_song.py
@@ -1,19 +1,18 @@
 import pytest
 
-from song import get_player_name
+from song import get_player_name,get_song_id
 
 def test_get_player_name_supported_player_found(mocker):
-    mocked_check_output = mocker.patch('song.subprocess.check_output',return_value='Qcm')
+    mocker.patch('song.subprocess.check_output',return_value='Qcm')
     SUPPERTED_PLAYER=['Qcm']
     player_name = get_player_name(SUPPERTED_PLAYER)
-    mocked_check_output.assert_called_once_with('playerctl --list-all', shell=True, text=True)
     assert player_name in SUPPERTED_PLAYER
 
 def test_get_player_name_supported_player_not_found(mocker):
     mocker.patch('song.subprocess.check_output',return_value="No players found")
     SUPPERTED_PLAYER=['Qcm']
-    with pytest.raises(ValueError):
-        get_player_name(SUPPERTED_PLAYER)
+    player_name = get_player_name(SUPPERTED_PLAYER)
+    assert player_name is None
 
 def test_get_player_name_empty_supported_player_not_found(mocker):
     mocker.patch('song.subprocess.check_output',return_value="Qcm")
